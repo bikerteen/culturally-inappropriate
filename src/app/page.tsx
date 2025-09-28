a734735135d393d2316f5436bd63355c08edfb3c
Razik Darji <raz@Raziks-MacBook-Pro.local>
2025-09-28 15:57:14 +0100

    changed the font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,9 @@ const Jerseyfont = Jersey_10({
 
 export default function Home() {
   return (
-    <div className="${Jerseyfont.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className={Jerseyfont.className + " grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20"}>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1>This is Culturally Inappropriate</h1>
+        <h1 className="text-4xl">This is Culturally Inappropriate.</h1>
       </main>
     </div>
   );
